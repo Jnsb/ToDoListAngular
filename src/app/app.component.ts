1c8027b2aaf1b3d46078b4d7325efc6c16d63c6b
Jean Bispo <jnsbpac@gmail.com>
2018-07-03 22:20:56 -0300

    ye

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nome = "";
+
+  alunos: String[] = [];
+  
+
+adicionar(){
+  let nome = this.alunos.push(this.nome);
+}
+
+excluir(param){
+  let remove = this.alunos.splice(param,1);
+}
+
 }
